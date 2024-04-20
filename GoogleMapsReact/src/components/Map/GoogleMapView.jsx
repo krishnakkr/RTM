d7a883ee58668c5f05c30ff9e322c0791bb67b6c
Nikhil Kumar V <nikhil.kumarv@xperi.com>
2024-04-20 23:40:46 +0530

    fixed Icons, and enabled button to change routs

--- GoogleMapsReact/src/components/Map/GoogleMapView.jsx
+++ GoogleMapsReact/src/components/Map/GoogleMapView.jsx
@@ -1,8 +1,75 @@
-import { GoogleMap, Marker, Polyline, useJsApiLoader} from '@react-google-maps/api';
-import DustBinPng from '../../assets/Images/Dustbin.png'
+import { DirectionsRenderer, GoogleMap, Marker, OverlayView, Polyline, useJsApiLoader } from '@react-google-maps/api';
+import DustBinPng from '../../assets/Images/Dustbin.png';
+import DumpYard from '../../assets/Images/DumpYard.png';
+import GarbageVan from '../../assets/Images/GarbageVan.png'
 import { useEffect, useState } from 'react';
 
 export default function GoogleMapView() {
+
+  const [markerComponent, setMarkerComponent] = useState(null);
+  
+  const [markerItems, setMarkerItems] = useState(
+    [
+      {
+        name: "mittaganahalli_DumpYard",
+        location: {
+          lat: 13.1216,
+          lng: 77.6207,
+        },
+        percentage: -1
+      },
+      {
+        name: "Bellahalli",
+        location: {
+          lat: 13.1005,
+          lng: 77.6427,
+        },
+        percentage: 50
+      },
+      {
+        name: "Kannuru",
+        location: {
+          lat: 13.0927,
+          lng: 77.6547,
+        },
+        percentage: 58
+      },
+      // {
+      //   name: "Yerappanahalli",
+      //   location: {
+      //     lat: 13.0865,
+      //     lng: 77.6862,
+      //   },
+      //   percentage: 58
+      // },
+      {
+        name: "Billeshavale",
+        location: {
+          lat: 13.0544,
+          lng: 77.6709,
+        },
+        percentage: 58
+      },
+      {
+        name: "Narayanapura",
+        location: {
+          lat: 13.0629,
+          lng: 77.6450,
+        },
+        percentage: 58
+      },
+      {
+        name: "Sampigehalli_GarbageVan",
+        location: {
+          lat: 13.0804,
+          lng: 77.6208,
+        },
+        percentage: -1
+      },
+    ]
+  );
+  const [reCreatePath, setReCreatePath] = useState(0);
+  
 
   const { isLoaded } = useJsApiLoader({
     id: import.meta.env.VITE_GOOGLE_MAP_API_KEY,
@@ -11,103 +78,170 @@ export default function GoogleMapView() {
 
   const containerStyle = {
     width: '100vw',
-    height: '100vh'
+    height: '90vh'
   };
   
   const center = {
-    lat: 12.9716,
-    lng: 77.5946
+    lat: 13.0927,
+    lng: 77.6547
   };
 
-  const markers = [
+  const UpdatedMarkers = [
     {
-      name: "Marathalli",
+      name: "mittaganahalli_DumpYard",
       location: {
-        lat: 12.9569,
-        lng: 77.7011,
+        lat: 13.1216,
+        lng: 77.6207,
       },
+      percentage: -1
     },
     {
-      name: "Majestic",
+      name: "Bellahalli",
       location: {
-        lat: 12.9767,
-        lng: 77.5713,
+        lat: 13.1005,
+        lng: 77.6427,
       },
+      percentage: 50
     },
     {
-      name: "Yelahanka",
+      name: "Kannuru",
       location: {
-        lat: 13.1155,
-        lng: 77.6070,
+        lat: 13.0927,
+        lng: 77.6547,
       },
+      percentage: 58
     },
-
+    {
+      name: "Yerappanahalli",
+      location: {
+        lat: 13.0865,
+        lng: 77.6862,
+      },
+      percentage: 58
+    },
+    {
+      name: "Billeshavale",
+      location: {
+        lat: 13.0544,
+        lng: 77.6709,
+      },
+      percentage: 58
+    },
+    {
+      name: "Narayanapura",
+      location: {
+        lat: 13.0629,
+        lng: 77.6450,
+      },
+      percentage: 58
+    },
+    {
+      name: "Sampigehalli_GarbageVan",
+      location: {
+        lat: 13.0804,
+        lng: 77.6208,
+      },
+      percentage: -1
+    },
   ];
+  
+
+  const generateMarkerComponent = async () => {
+    const items = await Promise.all(markerItems.map(async (item) => {
+      return (
+        <div key={item.name}>
+          <Marker position={item.location} 
+            options={{
+              icon: {
+                url: (item.name==='mittaganahalli_DumpYard' ? DumpYard : (item.name==='Sampigehalli_GarbageVan') ? GarbageVan : DustBinPng),
+                scaledSize: new window.google.maps.Size(100, 100) 
+              }
+            }}
+          />
+        {item.percentage !== -1 && <OverlayView
+            position={item.location}
+            mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
+            getPixelPositionOffset={(width, height) => ({
+              x: -(width / 2),
+              y: -(height / 2),
+            })}
+          >
+          <div style={{ position: 'absolute', background: 'black', padding: '5px', borderRadius: '5px', boxShadow: '0px 2px 6px rgba(0, 0, 0, 0.1)', }}>
+            <span style={{ color: 'white' }}>{`${item.percentage}%`}</span>
+          </div>
+        </OverlayView>
+          }
+        </div>
+      );
+    }));
+    setMarkerComponent(items);
+  };
+
+ 
 
   const [directions, setDirections] = useState(null);
+  
   useEffect(() => {
-    if (isLoaded) {
+    setDirections(prev=>null);
+    if (isLoaded && markerItems.length>0) {
       const directionsService = new window.google.maps.DirectionsService();
 
-      const waypoints = markers.map(marker => ({
+      const waypoints = markerItems.map(marker => ({
         location: marker.location,
         stopover: true
       }));
 
       directionsService.route(
         {
-          origin: markers[0].location,
-          destination: markers[markers.length - 1].location,
+          origin: markerItems[0].location,
+          destination: markerItems[markerItems.length - 1].location,
           waypoints: waypoints.slice(1, -1), 
           travelMode: window.google.maps.TravelMode.DRIVING
         },
         (result, status) => {
           if (status === window.google.maps.DirectionsStatus.OK) {
-            setDirections(result);
+            setDirections(prev=>result);
           } else {
             console.error(`Error fetching directions: ${status}`);
           }
         }
       );
+
+      
     }
-  }, [isLoaded]);
+    generateMarkerComponent();
+  }, [isLoaded, reCreatePath]);
+
+  useEffect(()=>{
+    generateMarkerComponent(); 
+    setReCreatePath(prev=>prev+1);
+  },[markerItems])
+
 
 
   return isLoaded && (
+    <>
     <GoogleMap
       mapContainerStyle={containerStyle}
       center={center}
-      zoom={10}
+      zoom={13}
     >
+      
+      { markerComponent}
 
-    {markers.map((item)=>{
-        return (
-          <div key={item.name}>
-            <Marker position={item.location} 
+         {directions && (
+            <DirectionsRenderer 
+            directions={directions}
             options={{
-              icon: {
-                url: DustBinPng,
-                scaledSize: new window.google.maps.Size(100, 100) 
-              }
+              suppressMarkers: true 
             }}
             />
-          </div>
-        )})
-      }
-
-      {directions && (
-        <Polyline
-          path={directions.routes[0].overview_path}
-          options={{
-            strokeColor: "#FF0000",
-            strokeOpacity: 1.0,
-            strokeWeight: 2,
-          }}
-        />
-      )}
-
-      
+          )}
 
     </GoogleMap>
-) 
+
+    <button className='bg-red-200 w-24 h-10' onClick={()=>{setMarkerItems(UpdatedMarkers);}}>click</button>
+
+   </>
+  );
 }
